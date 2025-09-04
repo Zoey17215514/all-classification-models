8f68d3762c05327dda158771ada601a849e94624
Zoey17215514 <141092895+Zoey17215514@users.noreply.github.com>
2025-09-04 17:08:51 +0800

    Update all_model.py

--- all_model.py
+++ all_model.py
@@ -1,7 +1,5 @@
 import streamlit as st
 from joblib import load
-import numpy as np
-import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
